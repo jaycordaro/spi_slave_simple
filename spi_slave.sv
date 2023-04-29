14253ac10a35654a57d4067dd0ac7ea37bc30dcd
Jay Cordaro <58152358+jaycordaro@users.noreply.github.com>
2023-04-29 15:07:09 -0700

    Update spi_slave.sv

--- spi_slave.sv
+++ spi_slave.sv
@@ -1,5 +1,5 @@
 /* BSD 2-Clause License
-	Copyright (c) 2020, 2021 Jay Cordaro
+	Copyright (c) 2020, 2021, 2023 Jay Cordaro
 	All rights reserved.
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
